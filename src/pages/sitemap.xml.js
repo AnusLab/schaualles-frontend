826714104ea3e6851715c64a8786fa0c43c07480
unknown <rattenbombe@mail.ru>
2025-12-06 01:44:26 +0100

    Fix SEO: Remove sitemap noindex header and update robots.txt

--- src/pages/sitemap.xml.js
+++ src/pages/sitemap.xml.js
@@ -95,8 +95,7 @@ export async function GET({ site }) {
         return new Response(sitemap, {
             headers: {
                 'Content-Type': 'application/xml; charset=utf-8',
-                'Cache-Control': 'public, max-age=3600, s-maxage=3600',
-                'X-Robots-Tag': 'noindex'
+                'Cache-Control': 'public, max-age=3600, s-maxage=3600'
             }
         });
 
